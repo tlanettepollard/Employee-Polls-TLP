2a1d78ba43a146affc90a69920597241656c7c5c
T. Lanette <TLPollard4516@gmail.com>
2022-06-16 13:38:17 -0400

    Starting again
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from '../src/components/App';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import middleware from './middleware';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, middleware);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
+export function formatDate(timestamp) {
+	const d = new Date(timestamp);
+	const time = d.toLocaleTimeString('en-US');
+	return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString();
+}
+
 export function formatQuestion(question, author, authedUser) {
 	const { id, timestamp, optionOne, optionTwo } = question;
 	const { name, avatarURL } = author;
