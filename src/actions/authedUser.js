3ffde5bb892d9bfd50a7e8c3f9abf12e3a39659d
T. Lanette <TLPollard4516@gmail.com>
2022-06-16 18:25:10 -0400

    Added Reducers
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,8 +1,17 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER';
+export const SIGNED_OUT_USER = 'SIGNED_OUT_USER';
 
+// Action creator for signed in user
 export function setAuthedUser(id) {
 	return {
 		type: SET_AUTHED_USER,
 		id,
+	};
+}
+
+// Action creator to sign out current user
+export function signedOutUser() {
+	return {
+		type: SIGNED_OUT_USER,
 	};
 }
