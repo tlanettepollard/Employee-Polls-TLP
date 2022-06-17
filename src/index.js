985389fec8cfa941af1b3fd5be6cede2b1d9d073
T. Lanette <TLPollard4516@gmail.com>
2022-06-16 21:36:24 -0400

    Added middleware
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from '../src/components/App';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
+import middleware from './middleware';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, middleware);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import authedUser from './authedUser';
 import users from './users';
 import questions from './questions';
 
+// CreateStore only accepts a single reducer 
 export default combineReducers({
 	authedUser,
 	questions,
