5545249ec37e47ada2af0ca88ae432ee9a3dff75
T. Lanette <TLPollard4516@gmail.com>
2022-06-18 17:50:21 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
-import { useEffect, Fragment } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useEffect } from 'react';
 import { handleInitialData } from '../actions/shared';
 import { connect } from 'react-redux';
+import { LoadingBar } from 'react-redux-loading';
 import '../App.css';
-import logo from '../images/companyavatar.png';
+import { Container } from 'react-bootstrap';
+//import Login from './Login';
+
 
 function App(props) {
 	useEffect(() => {
 		props.dispatch(handleInitialData());
-	}, [props]);
+	}, [props, props.authedUser]);
 
 	return (
-		<Fragment>
-			<div className='container'>
-				<h1 className="app-heading">Employee Polls</h1>
-				<img src={logo} className='app-logo' alt='logo' />
-			</div>
-		</Fragment>
+		<Router>
+			<LoadingBar />
+			<Container>
+				<h1 className='app-heading'>Employee Polls</h1>
+				
+			</Container>
+		</Router>
 	);
-	
 }
-const maptStateToProps = ({ questions }) => ({
-	loading: questions === null,
-})
 
-export default connect(maptStateToProps)(App);
+const mapStateToProps = ({ authedUser }) => ({
+	loading: authedUser === null,
+});
+
+export default connect(mapStateToProps)(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import authedUser from './authedUser';
 import users from './users';
 import questions from './questions';
+import { loadingBarReducer } from 'react-redux-loading';
 
-// CreateStore only accepts a single reducer 
+// CreateStore only accepts a single reducer
 export default combineReducers({
 	authedUser,
 	questions,
 	users,
+	loadingBar: loadingBarReducer,
 });
