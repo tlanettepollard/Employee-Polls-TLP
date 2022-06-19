782b2bcfd76b8f5d7233613976b1c8350c80d452
T. Lanette <TLPollard4516@gmail.com>
2022-06-19 13:44:46 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -8,19 +8,20 @@ import '../App.css';
 import { Container } from 'react-bootstrap';
 //import Login from './Login';
 
-
 function App(props) {
 	useEffect(() => {
 		props.dispatch(handleInitialData());
 	}, [props, props.authedUser]);
 
+
 	return (
 		<Router>
-			<LoadingBar />
-			<Container>
-				<h1 className='app-heading'>Employee Polls</h1>
-				
-			</Container>
+			<div className='app'>
+				<LoadingBar />
+				<Container>
+					<h1 className='app-heading'>Employee Polls</h1>
+				</Container>
+			</div>
 		</Router>
 	);
 }
