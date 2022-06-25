2e4db4ea846a44dc784c9e3145fcab3fb3569c0f
T. Lanette <TLPollard4516@gmail.com>
2022-06-25 15:26:27 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,5 +1,5 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER';
-export const SIGNED_OUT_USER = 'SIGNED_OUT_USER';
+export const REMOVE_AUTHED_USER = 'REMOVE_AUTHED_USER';
 
 // Action creator for signed in user
 export function setAuthedUser(id) {
@@ -10,10 +10,8 @@ export function setAuthedUser(id) {
 }
 
 // Action creator to sign out current user
-export function signedOutUser() {
+export function removeAuthedUser() {
 	return {
-		type: SIGNED_OUT_USER,
+		type: REMOVE_AUTHED_USER,
 	};
 }
-
-
