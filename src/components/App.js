146f5705c70d15c8393291eaeebbd3ca597f3dfe
T. Lanette <TLPollard4516@gmail.com>
2022-06-26 21:06:46 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { handleInitialData } from '../actions/shared';
 import { connect } from 'react-redux';
 //import { Route, Routes } from 'react-router-dom';
 import { LoadingBar } from 'react-redux-loading';
-//import PageNavbar from './PageNavbar';
+//import Navigation from './Navigation';
 import Login from './Login';
 //import Home from './Home';
 //import PollPage from './PollPage';
@@ -12,12 +12,11 @@ import Login from './Login';
 import Container from 'react-bootstrap/Container';
 import '../App.css';
 
+
 const App = (props) => {
 	useEffect(() => {
 		props.dispatch(handleInitialData());
 	}, [props, props.authedUser]);
-
-	
 
 	return (
 		<div className='App'>
