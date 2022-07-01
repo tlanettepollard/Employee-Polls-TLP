fec302669b70186c47364bc2d201b5c4c287acc7
T. Lanette <TLPollard4516@gmail.com>
2022-07-01 18:03:40 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,14 +1,18 @@
 import { getInitialData } from '../utils/api';
-import { receiveQuestions } from './questions';
+import { groupQuestions, receiveQuestions } from './questions';
 import { receiveUsers } from './users';
 import { showLoading, hideLoading } from 'react-redux-loading-bar';
 
 export function handleInitialData() {
-	return (dispatch) => {
+	return (dispatch, getState) => {
 		dispatch(showLoading());
 		return getInitialData().then(({ users, questions }) => {
 			dispatch(receiveQuestions(questions));
 			dispatch(receiveUsers(users));
+			const { authedUser } = getState();
+			if (authedUser != null) {
+				dispatch(groupQuestions(authedUser));
+			}
 			dispatch(hideLoading());
 		});
 	};
