c9090c52cfd10d2f5ab9cd0337916385ad809ebe
T. Lanette <TLPollard4516@gmail.com>
2022-06-30 23:25:14 -0400

    Updates: reducer
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,8 +1,12 @@
-import { SET_AUTHED_USER } from '../actions/authedUser';
+import { SET_AUTHED_USER, REMOVE_AUTHED_USER } from '../actions/authedUser';
 
 export default function authedUser(state = null, action) {
-	if (action.type === SET_AUTHED_USER) {
-		return action.id;
+	switch (action.type) {
+		case SET_AUTHED_USER:
+			return action.id;
+		case REMOVE_AUTHED_USER:
+			return null;
+		default:
+			return state;
 	}
-	return state;
 }
